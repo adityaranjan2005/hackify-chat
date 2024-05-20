8e2b077181a4aaf60e6de650e270fb622adbc37a
AdityaRanjan2005 <145277402+AdityaRanjan2005@users.noreply.github.com>
2024-05-20 09:52:24 +0530

    first commit

---
+++ config/chatrooms.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    name: 'Web Island',
+    image: 'chatrooms/terminus.jpg'
+  },
+  {
+    name: 'ML Island',
+    image: 'chatrooms/alexandria.jpg'
+  },
+  {
+    name: 'Web 3 Island',
+    image: 'chatrooms/sanctuary.jpg'
+  },
+  {
+    name: 'Algo Island',
+    image: 'chatrooms/hilltop.jpg'
+  }
+]
+
